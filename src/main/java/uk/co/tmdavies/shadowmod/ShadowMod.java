db0a7f79c58178a49e0a8a8aad5607d5013f4600
shadowty1232 <tyl@null.net>
2022-09-02 22:48:48 +0100

    Initial commit

---
+++ src/main/java/uk/co/tmdavies/shadowmod/ShadowMod.java
@@ -0,0 +1,46 @@
+package uk.co.tmdavies.shadowmod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+import uk.co.tmdavies.shadowmod.utils.ModConstants;
+
+@Mod(ModConstants.MOD_ID)
+public class ShadowMod {
+
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public ShadowMod() {
+
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+        LOGGER.info("ShadowMod is loading...");
+
+    }
+
+    @Mod.EventBusSubscriber(modid = ModConstants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+
+
+        }
+
+    }
+
+}
---
+++ src/main/java/uk/co/tmdavies/shadowmod/utils/ModConstants.java
@@ -0,0 +1,7 @@
+package uk.co.tmdavies.shadowmod.utils;
+
+public class ModConstants {
+
+    public static final String MOD_ID = "shadowmod";
+
+}
